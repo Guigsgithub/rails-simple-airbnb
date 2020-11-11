e829c60c73cab842e1d525d1a0f8e6dcfd8cb2f9
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-11 16:54:32 +0100

    creates form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,4 +7,19 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to @flat, notice: 'Flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
